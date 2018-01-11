79a3e62a9892e1fdac4deba92df5fa9909a58545
msukkari <michael.sukkarieh@mail.mcgill.ca>
2018-01-10 23:16:12 -0500

    added basic movement and collision. TODO: map raw resource to it's associated resource for mining to work

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -6,18 +6,45 @@ public class PlayerController : MonoBehaviour {
 
     public float m_speed;
 
+    private Rigidbody2D rb;
+    private GameObject curRawResource;
+
 	// Use this for initialization
 	void Start () {
         ControllerInputManager.GetInstance().OnADown += OnAPress;
+
+        rb = GetComponent<Rigidbody2D>();
+        curRawResource = null;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.Space)) Debug.Log("Player pressed space");
+	void FixedUpdate () {
+
+        float y = Input.GetKey(KeyCode.W) ? 1.0f : Input.GetKey(KeyCode.S) ? -1.0f : 0.0f;
+        float x = Input.GetKey(KeyCode.D) ? 1.0f : Input.GetKey(KeyCode.A) ? -1.0f : 0.0f;
+        rb.velocity = new Vector2(x * m_speed, y * m_speed);
+
+        if (Input.GetKeyDown(KeyCode.Space) && curRawResource) mineRR(curRawResource);
+    }
+
+    public void OnCollisionEnter2D(Collision2D coll)
+    {
+        // NOTE: this might cause issues when player is colliding with two raw resources!
+        if (coll.gameObject.tag == "RR") curRawResource = coll.gameObject;
+    }
+
+    public void OnCollisionExit2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "RR") curRawResource = null;
     }
 
     private void OnAPress()
     {
         Debug.Log("Player pressed A");
+    }
+
+    private void mineRR(GameObject rr)
+    {
+
     }
 }
--- Assets/Scripts/ResourceSystem/Resource.cs
+++ Assets/Scripts/ResourceSystem/Resource.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Resource : MonoBehaviour {
 
-	public enum Type { Alpha, Beta, Gamma };
+	public enum Type { RMetal, ROil, RRubber, Metal, Oil, Rubber };
     public enum State { Blocked, Free }; // Resource is blocked when it is in a machine or is in a raw resource block
 
     public Type m_type;
@@ -14,6 +14,17 @@ public class Resource : MonoBehaviour {
     {
         m_type = type;
         m_state = state;
+    }
+
+    public void OnMine()
+    {
+        if(m_type == Type.Metal || m_type == Type.Oil || m_type == Type.Rubber || m_state == State.Free)
+        {
+            Debug.Log("ERROR: Attempting to mine a resource that isn't raw!");
+            return;
+        }
+
+        //GameObject fr = 
     }
 
 }
