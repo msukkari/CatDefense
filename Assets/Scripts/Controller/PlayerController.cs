cd0f35a68ca8c28134485ef00e1f5040a7ca6d23
kartoshka <eli.harfouche@gmail.com>
2018-01-11 19:37:45 -0500

    Set up controller for player

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -11,7 +11,8 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody2D rb;
 
 	void Start () {
-        ControllerInputManager.GetInstance().OnADown += OnAPress;
+        ControllerInputManager.GetInstance().OnADown += Pickup;
+        ControllerInputManager.GetInstance().OnLSChange += Move;
 
         rb = GetComponent<Rigidbody2D>();
         neighborObject = null;
@@ -20,17 +21,30 @@ public class PlayerController : MonoBehaviour {
 	
 	void FixedUpdate () {
 
-        float y = Input.GetKey(KeyCode.W) ? 1.0f : Input.GetKey(KeyCode.S) ? -1.0f : 0.0f;
-        float x = Input.GetKey(KeyCode.D) ? 1.0f : Input.GetKey(KeyCode.A) ? -1.0f : 0.0f;
-        rb.velocity = new Vector2(x * m_speed, y * m_speed);
+        //float y = Input.GetKey(KeyCode.W) ? 1.0f : Input.GetKey(KeyCode.S) ? -1.0f : 0.0f;
+        //float x = Input.GetKey(KeyCode.D) ? 1.0f : Input.GetKey(KeyCode.A) ? -1.0f : 0.0f;
+        //rb.velocity = new Vector2(x * m_speed, y * m_speed);
 
-        if (Input.GetKeyDown(KeyCode.Space) && neighborObject)
+        //if (Input.GetKeyDown(KeyCode.Space) && neighborObject)
+        //{
+        //    if (neighborObject.tag == "RR") mineRR(neighborObject);
+        //    else if (neighborObject.tag == "Refiner") neighborObject.GetComponent<Refiner>().Refine(heldResource);
+        //}
+    }
+
+    public void Move(float x, float y)
+    {
+        rb.velocity = new Vector2(x * m_speed, y * m_speed);
+    }
+
+    public void Pickup()
+    {
+        if(neighborObject)
         {
             if (neighborObject.tag == "RR") mineRR(neighborObject);
             else if (neighborObject.tag == "Refiner") neighborObject.GetComponent<Refiner>().Refine(heldResource);
         }
     }
-
     public void OnCollisionEnter2D(Collision2D coll)
     {
         // NOTE: this might cause issues when player is colliding with two raw resources!
