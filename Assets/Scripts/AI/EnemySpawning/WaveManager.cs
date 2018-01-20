8ad3b55048b3741b3ea2e9a84e106ac29f5fe623
Kartoshka <eli.harfouche@gmail.com>
2018-01-20 15:28:09 -0500

    First mini pass on wave manager, fix some visual things

---
+++ Assets/Scripts/AI/EnemySpawning/WaveManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Wave{
+	public List<SpawnPoints> SpawnPoints;
+}
+
+[System.Serializable]
+public struct SpawnPoints
+{
+	public GameObject spawnPoint;
+	public List<Spawn> enemiesToSpawn;
+}
+
+[System.Serializable]
+public struct Spawn
+{
+	public GameObject enemy;
+	public float count;
+	public float timeBetween;
+}
+
+public class WaveManager : MonoBehaviour {
+
+	public List<Wave> waves;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
