6fb548121a8c72ea259f09e9f216f8bc0e48b23a
Kartoshka <eli.harfouche@gmail.com>
2018-01-22 15:00:18 -0500

    Add resource amount indicator

---
+++ Assets/Scripts/UI/ResourceUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ResourceUI : MonoBehaviour {
+
+	public Text metal;
+	public Text oil;
+	public Text rubber;
+
+	public Player player;
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<Player> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		metal.text = player.metal.ToString();
+		oil.text = player.oil.ToString();
+		rubber.text = player.rubber.ToString();
+	}
+}
