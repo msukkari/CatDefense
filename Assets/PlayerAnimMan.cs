626e92374f4876558777dcf20a346aef9f2adeb7
kartoshka <eli.harfouche@gmail.com>
2018-01-22 21:51:17 -0500

    Animate player

---
+++ Assets/PlayerAnimMan.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+public class PlayerAnimMan : MonoBehaviour {
+
+    Rigidbody2D m_rb;
+    Animator m_anim;
+	// Use this for initialization
+	void Start () {
+        m_rb = GetComponent<Rigidbody2D>();
+        m_anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_anim.SetFloat("WalkX", m_rb.velocity.normalized.x);
+        m_anim.SetFloat("WalkY", m_rb.velocity.normalized.y);
+
+    }
+}
