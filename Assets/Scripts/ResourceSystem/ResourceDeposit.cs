82a8c3cb1da65dc2839acaeb7237c755fced3b3e
Kartoshka <eli.harfouche@gmail.com>
2018-01-23 17:45:18 -0500

    Fix death of resource deposit

--- Assets/Scripts/ResourceSystem/ResourceDeposit.cs
+++ Assets/Scripts/ResourceSystem/ResourceDeposit.cs
@@ -12,19 +12,29 @@ public class ResourceDeposit : MonoBehaviour {
     public GameObject m_interactArea;
     public IInteractable m_ugInteract;
 
+	private HealthComponent m_health;
     // Use this for initialization
     void Start () {
+		m_health = this.GetComponent<HealthComponent> ();
+		if (m_health != null)
+		{
+			m_health.OnDeath += Die;
+		}
         m_player = m_playerObject.GetComponent<Player>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Die()
+	{
+		SceneManager.LoadScene ("menu");
 	}
 
 	void OnDestroy()
 	{
-		SceneManager.LoadScene ("menu");
-		//Gameover
+		if (m_health != null)
+		{
+			m_health.OnDeath -= Die;
+		}
 	}
+
 }
